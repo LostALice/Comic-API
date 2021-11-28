6a3b53628a04b132662f493c0c0e23f180c14cbc
LostALice <48596994+LostALice@users.noreply.github.com>
2021-11-28 14:24:25 +0800

    Update api.py

--- api.py
+++ api.py
@@ -6,7 +6,23 @@ from bs4 import BeautifulSoup
 from os import path,makedirs
 
 class nhentai:
-    def __init__(self, code: int):
+    def __init__(self):
+        self.code = None
+        self.info = None
+        self.title = None
+        self.sub_title = None
+        self.parodies = None
+        self.tag = None
+        self.artists = None
+        self.groups = None
+        self.languages = None
+        self.categories = None
+        self.pages = None
+        self.uploaded = None
+        self.popular = None
+        self.comics = None
+
+    def infos(self, code: int = None):
         """
         Get the doujinshi information from nhentai.net
 
@@ -16,6 +32,7 @@ class nhentai:
         self.info = All information
 
         {
+            "title" " "Title"
             "parodies" : "Parodies"
             "tag" : "Tags"
             "artists" : "Artists"
@@ -27,11 +44,12 @@ class nhentai:
         }
 
         """
-        self.code = code
-        self.comic_set = {}
 
-        if not isinstance(code, int):
-            raise BaseException("Value should be integer")
+        try:
+            self.code = int(code)
+        except Exception as e:
+            print(e)
+            raise BaseException("Code incorrect")
 
         ua = UserAgent()
         header = {'user-agent': ua.random}
@@ -43,6 +61,13 @@ class nhentai:
         page = soup.find_all(class_="tag-container field-name")
 
         json = {"Code": code}
+        json["Title"] = [item.text for item in soup.find_all("h1", attrs={"class": "title"})[0]]
+
+        try:
+            json["Sub_Title"] = [item.text for item in soup.find_all("h2", attrs={"class": "title"})[0]]
+        except:
+            json["Sub_Title"] = None
+
         for p in page:
             attr = p.contents[0].replace("\n\t\t\t\t\t\t\t\t", "").replace(":", "")
             tags = p.find_all("span", attrs={"class": "name"})
@@ -58,11 +83,13 @@ class nhentai:
         time = soup.find_all("time", attrs={"class": "nobold"})
         json["Uploaded"] = [time[0].text]
 
-        info_staff = ["Parodies","Tags","Artists","Groups","Languages","Categories","Pages","Uploaded"]
+        info_staff = ["Parodies","Tags","Artists","Groups","Languages","Categories","Pages","Uploaded","Sub_Title","Title"]
         for staff in info_staff:
-            if not staff in json:json[staff] = None
+            if not staff in json: json[staff] = None
 
         self.info = json
+        self.title = json["Title"]
+        self.sub_title = json["Sub_Title"]
         self.parodies = json["Parodies"]
         self.tag = json["Tags"]
         self.artists = json["Artists"]
@@ -72,15 +99,25 @@ class nhentai:
         self.pages = json["Pages"]
         self.uploaded = json["Uploaded"]
 
-    def image(self,thumbnail=False):
-        self.comic_set = {}
+        return self.info
+
+    def image(self,code: int = None, thumbnail: bool = False):
+        self.comics = {}
         """
         Get the doujinshi base64 encoded string from nhentai.net
 
         Returns:
-           self.comic_set: {"path":"base64 string"}
+           self.comics: {"path":"<base64 string>"}
         """
-        code = self.code
+
+        if code == None:
+            try:
+                self.code = int(code)
+            except Exception as e:
+                print(e)
+                raise BaseException("Code incorrect")
+
+
         pages = BeautifulSoup(requests.get(f"https://nhentai.net/g/{code}/").text, "lxml").find_all(class_="thumb-container")
         image = [page.find_all("img", attrs={"class": "lazyload"})[0]["data-src"] for page in pages]
 
@@ -104,7 +141,7 @@ class nhentai:
                     src_ = src.replace("t.",".").replace("https://.nhentai.net/galleries/", "")
                 try:
                     chunk = await resp.content.read()
-                    self.comic_set[src_] = base64.b64encode(chunk).decode("utf-8")
+                    self.comics[src_] = base64.b64encode(chunk).decode("utf-8")
                 except Exception as e:
                     print(f"{src}:{e}")
                     tasks.append(asyncio.create_task(fetch(link, session)))
@@ -114,9 +151,9 @@ class nhentai:
         loop.run_until_complete(main(image))
         loop.close()
 
-        return self.comic_set
+        return self.comics
 
-    def download(self,path_: str = "./", thumbnail: bool = False):
+    def download(self,code: int = None, path_: str = "./", thumbnail: bool = False):
         """
         Download and save the image to local directory
 
@@ -124,10 +161,44 @@ class nhentai:
             path (str): Local directory
         """
 
-        img = self.image()
+        if code == None:
+            try:
+                self.code = int(code)
+            except Exception as e:
+                print(e)
+                raise BaseException("Code incorrect")
+
+        img = self.image(code)
         for i in img:
             a = i.replace("/", "-")
-            if not path.exists(f"{path_}{self.code}"):
-                makedirs(f"{path_}{self.code}")
+            if not path_.endswith("/"): path_ += "/"
+            if not path.exists(f"{path_}{self.code}"): makedirs(f"{path_}{self.code}")
+
             with open(f"{path_}{self.code}/{a}", "wb") as f:
                 f.write(base64.b64decode(img[i]))
+
+    def popular_now(self) -> list:
+        """
+        Get todays popular doujinshi
+
+        Returns:
+            list: self.popular
+        """
+
+        soup = BeautifulSoup(requests.get("https://nhentai.net").text, "lxml")
+        page = [p for p in soup.find_all("a", attrs={"class": "cover"})[:5]]
+        self.popular = [p.attrs["href"].replace("/","").replace("g","") for p in page]
+        return self.popular
+
+    def latest(self) -> int:
+        """
+        Get the latest doujinshi
+
+        Returns:
+            int: self.latest
+        """
+
+        soup = BeautifulSoup(requests.get("https://nhentai.net").text, "lxml")
+        page = soup.find_all("a", attrs={"class": "cover"})[5]
+        self.latest = int(page.attrs["href"].replace("/","").replace("g",""))
+        return self.latest
